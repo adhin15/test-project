49bcaefa4ffba645d65e83c1c710a13d2bcb86a0
Adhin <adhin.alifarchan@ist.id>
2025-01-21 19:00:01 +0700

    meta data

--- src/components/layouts/general/applayout/Layout.tsx
+++ src/components/layouts/general/applayout/Layout.tsx
@@ -7,7 +7,7 @@ import "../../../../app/globals.css";
 import "aos/dist/aos.css";
 import { GlobalContext } from "./layout.context";
 
-import type { Viewport } from "next";
+import type { Metadata, Viewport } from "next";
 import useLayout from "./Layout.hook";
 import LayoutProvider from "./Layout.provider";
 import { useEffect } from "react";
@@ -15,6 +15,21 @@ import { useEffect } from "react";
 export const viewport: Viewport = {
   maximumScale: 1.0,
   userScalable: false,
+};
+export const metadata: Metadata = {
+  title: "MoFlixx - Movie Database",
+  description:
+    "Welcome to MoFlixx, explore information about movies and TV shows. You can browse through various titles, details like  as release dates, genres, and synopses, and watch trailers.",
+  icons: "https://mo-flixx.vercel.app/images/main-logo.webp",
+  openGraph: {
+    title: "MoFlixx - Movie Database",
+    siteName: "MoFlixx - Movie Database",
+    description:
+      "Welcome to MoFlixx, explore information about movies and TV shows. You can browse through various titles, details like  as release dates, genres, and synopses, and watch trailers.",
+    images: "https://mo-flixx.vercel.app/images/website-thumbnail.png",
+    type: "website",
+    url: "https://mo-flixx.vercel.app",
+  },
 };
 
 const Layout = ({
